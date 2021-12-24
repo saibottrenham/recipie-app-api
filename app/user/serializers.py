415267459d64b6ed2dfa1bc3daa85032d389cf56
Tobias Mahnert <tobiasmahnert@web.de>
2021-12-24 22:06:55 +1100

    added user managment endpoint and token generation

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -16,6 +16,15 @@ class UserSerializer(serializers.ModelSerializer):
             **validated_data
         )
 
+    def update(self, instance, validated_data):
+        """Update a user with encrypted password and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serialiser for the user authentication object"""
